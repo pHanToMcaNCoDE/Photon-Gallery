b6aab0c97f473db39e25651917dcaf338a409b29
Victory Odumeh <vodumeh@gmail.com>
2023-09-21 13:55:39 +0100

    ready for launch 🚀

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,47 +1,47 @@
 export const images = [
     {
         id: "1",
-        image: './src/assets/1.jpg',
+        image: '/assets/1.jpg',
         name: 'Winter Sleeveless Sweater',
     },
     {
         id: "2",
-        image: './src/assets/2.jpg',
+        image: '/assets/2.jpg',
         name: 'Tie',
     },
     {
         id: "3",
-        image: './src/assets/3.jpg',
+        image: '/assets/3.jpg',
         name: 'Winter Sweater',
     },
     {
         id: "4",
-        image: './src/assets/7.jpg',
+        image: '/assets/7.jpg',
         name: 'Alpha Camera',
     },
     {
         id: "5",
-        image: './src/assets/5.jpg',
+        image: '/assets/5.jpg',
         name: 'Blue Jeans',
     },
     {
         id: "6",
-        image: './src/assets/9.jpg',
+        image: '/assets/9.jpg',
         name: 'X-box Game Pad',
     },
     {
         id: "7",
-        image: './src/assets/4.jpg',
+        image: '/assets/6.jpg',
         name: 'Orange Towel',
     },
     {
         id: "8",
-        image: './src/assets/6.jpg',
+        image: '/assets/4.jpg',
         name: 'Blue Jean Pairs',
     },
     {
         id: "9",
-        image: './src/assets/8.jpg',
+        image: '/assets/8.jpg',
         name: 'Black Watch',
     },
 ]
